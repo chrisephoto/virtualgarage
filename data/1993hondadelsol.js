30de59af99e0f8b4c517b32fc4577ccb7b5c19e3
Chris <chrisephotography@gmail.com>
2022-07-22 14:52:00 -0400

    Create 1993hondadelsol.js

---
+++ data/1993hondadelsol.js
@@ -0,0 +1,60 @@
+title = "1993 Honda del Sol"
+
+parts = [
+  {
+    title: "Headlights",
+    options: 
+      {
+        description: "Stock",
+        url: "stock.jpg"
+      },
+      {
+        description: "Civic Type R",
+        url: "ctr.jpg"
+      },
+      {
+        description: "Yellow",
+        url: "yellow.jpg"
+      },
+      {
+        description: "Black",
+        url: "black.jpg"
+      }
+  },
+  {
+    title: "Front Bumper",
+    options:
+      {
+        description: "Stock",
+        url: "stock.jpg"
+      },
+      {
+        description: "Civic Type R",
+        url: "ctr.jpg"
+      },
+      {
+        description: "Mugen",
+        url: "mugen.jpg"
+      },
+      {
+        description: "Spoon",
+        url: "spoon.jpg"
+      }
+  },
+  {
+    title: "Rear Bumper",
+    options:
+      {
+        description: "Stock",
+        url: "stock.jpg"
+      },
+      {
+        description: "Civic Type R",
+        url: "ctr.jpg"
+      },
+      {
+        description: "Sergeant",
+        url: "sergeant.jpg"
+      }
+  }
+];
