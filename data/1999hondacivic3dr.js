f3a576f16513c091613e0a2c911240b26c8b1d20
Chris <chrisephotography@gmail.com>
2022-07-22 16:13:05 -0400

    Update 1999hondacivic3dr.js

--- data/1999hondacivic3dr.js
+++ data/1999hondacivic3dr.js
@@ -1,34 +1,33 @@
 title = "1999 Honda Civic (3DR)"
 
+
 parts = [
   { 
-    "Headlights": {
-      "options":
-      [
-        {
-          "description":"Stock",
-          "url":"stock.jpg"
-        },
-        {
-          "description":"Civic Type R",
-          "url":"stock.jpg"
-        }
-      ]
-    }
+    title: "Headlights",
+    options:
+    [
+      {
+        "description":"Stock",
+        "url":"stock.jpg"
+      },
+      {
+        "description":"Civic Type R",
+        "url":"stock.jpg"
+      }
+    ]
   },
-  {
-    "Front Bumper": {
-      "options":
-      [
-        {
-          "description":"Stock",
-          "url":"stock.jpg"
-        },
-        {
-          "description":"Civic Type R",
-          "url":"stock.jpg"
-        }
-      ]
-    }
+  { 
+    title: "Front Bumper",
+    options:
+    [
+      {
+        "description":"Stock",
+        "url":"stock.jpg"
+      },
+      {
+        "description":"Civic Type R",
+        "url":"stock.jpg"
+      }
+    ]
   }
 ];
