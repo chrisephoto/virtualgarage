fa4a2a67dd9bed1a2f37f5b016d9ac4cb1d86787
Chris <chrisephotography@gmail.com>
2022-07-22 16:26:03 -0400

    Update init.js

--- init.js
+++ init.js
@@ -4,24 +4,26 @@ function init() {
   var hash = window.location.hash.substr(1);
   dataScriptURL = "data/" + hash + ".js";
   loadScript(dataScriptURL, updatePage);
-  loadScript("game.js", null);
+  loadScript("game.js");
 }
 
 function loadScript(url, callback)
 {
-    // Adding the script tag to the head as suggested before
-    var head = document.head;
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
+  // Adding the script tag to the head as suggested before
+  var head = document.head;
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
 
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
+  // Then bind the event to the callback function.
+  // There are several events for cross browser compatibility.
+  if (callback) {
     script.onreadystatechange = callback;
     script.onload = callback;
+  }
 
-    // Fire the loading
-    head.appendChild(script);
+  // Fire the loading
+  head.appendChild(script);
 }
 
 var updatePage = function() {
