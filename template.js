a3908c15d06cd4b6615ea01ab9886f1e3497b1ca
Chris <chrisephotography@gmail.com>
2022-07-22 14:23:55 -0400

    Update template.js

--- template.js
+++ template.js
@@ -1,34 +1,61 @@
 parts = [
   {
-    title: "Dr Stone: Ryusui",
-    image: "images/dr-stone-ryusui.jpg"
+    title: "Headlights",
+    [
+      {
+        description: "Stock",
+        url: "stock.jpg"
+      },
+      {
+        description: "Civic Type R",
+        url: "ctr.jpg"
+      },
+      {
+        description: "Yellow",
+        url: "yellow.jpg"
+      },
+      {
+        description: "Black",
+        url: "black.jpg"
+      }
+    ]
   },
   {
-    title: "Urusei Yatsura",
-    image: "images/urusei-yatsura.jpg"
+    title: "Front Bumper",
+    [
+      {
+        description: "Stock",
+        url: "stock.jpg"
+      },
+      {
+        description: "Civic Type R",
+        url: "ctr.jpg"
+      },
+      {
+        description: "Mugen",
+        url: "mugen.jpg"
+      },
+      {
+        description: "Spoon",
+        url: "spoon.jpg"
+      }
+    ]
   },
   {
-    title: "The Quintessential Quintuplets the Movie",
-    image: "images/quintessential-quintuplets.jpg"
-  },
-  {
-    title: "SPY x FAMILY",
-    image: "images/spy-x-family.jpg"
-  },
-  {
-    title: "Sailor Moon R the Movie: Promise of the Rose",
-    image: "images/sailor-moon-r.jpg"
-  },
-  {
-    title: "MF Ghost",
-    image: "https://m.imageimg.net/upload/artist_img/MFGST-OFCL-0001/12aa1def131e9435519c4147ed52125a16d174ef_61c96ed270469.jpg"
-  },
-  {
-    title: "Attack on Titan: The Final Season",
-    image: "https://s1.zerochan.net/Attack.on.Titan.600.1417657.jpg"
-  },
-  {
-    title: "My Hero Academia",
-    image: "https://s1.zerochan.net/Boku.no.Hero.Academia.600.2139574.jpg"
+    title: "Rear Bumper",
+    [
+      {
+        description: "Stock",
+        url: "stock.jpg"
+      },
+      {
+        description: "Civic Type R",
+        url: "ctr.jpg"
+      },
+      {
+        description: "Sergeant",
+        url: "sergeant.jpg"
+      },
+    ]
   }
 ];
