d6642d820bb3c2602e5f071c6555454e42c3bd10
Chris <chrisephotography@gmail.com>
2022-07-22 15:45:03 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,3 @@
-document.title = title;
-document.getElementById("page-title").innerHTML = title;
-
 for (let j = 0; j < parts.length; j++) {
   document.getElementById("display").appendHTML = parts[j].title;
 }
