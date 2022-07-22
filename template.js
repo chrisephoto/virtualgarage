eb290c62ca7156a41d20dd732057dc1682c0009a
Chris <chrisephotography@gmail.com>
2022-07-22 12:50:44 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,34 @@
+parts = [
+  {
+    title: "Dr Stone: Ryusui",
+    image: "images/dr-stone-ryusui.jpg"
+  },
+  {
+    title: "Urusei Yatsura",
+    image: "images/urusei-yatsura.jpg"
+  },
+  {
+    title: "The Quintessential Quintuplets the Movie",
+    image: "images/quintessential-quintuplets.jpg"
+  },
+  {
+    title: "SPY x FAMILY",
+    image: "images/spy-x-family.jpg"
+  },
+  {
+    title: "Sailor Moon R the Movie: Promise of the Rose",
+    image: "images/sailor-moon-r.jpg"
+  },
+  {
+    title: "MF Ghost",
+    image: "https://m.imageimg.net/upload/artist_img/MFGST-OFCL-0001/12aa1def131e9435519c4147ed52125a16d174ef_61c96ed270469.jpg"
+  },
+  {
+    title: "Attack on Titan: The Final Season",
+    image: "https://s1.zerochan.net/Attack.on.Titan.600.1417657.jpg"
+  },
+  {
+    title: "My Hero Academia",
+    image: "https://s1.zerochan.net/Boku.no.Hero.Academia.600.2139574.jpg"
+  }
+];
