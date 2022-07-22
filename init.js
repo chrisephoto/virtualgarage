fc43bfe0c1b3be3c1eaa8bdf255a3af28b4c8e92
Chris <chrisephotography@gmail.com>
2022-07-22 15:27:32 -0400

    Update init.js

--- init.js
+++ init.js
@@ -5,14 +5,15 @@ function init() {
   var hash = window.location.hash.substr(1);
 
   //load parts list json
-  var script = document.createElement("script");
+  var dataScript = document.createElement("script");
   var src = "data/" + hash + ".js";
-  script.setAttribute("src", src);
-  script.setAttribute("type", "text/javascript");
-  document.getElementsByTagName('head')[0].appendChild(script);
+  dataScript.setAttribute("src", src);
+  dataScript.setAttribute("type", "text/javascript");
+  document.getElementsByTagName('head')[0].appendChild(dataScript);
 
   //load game.js
-  script.setAttribute("src", "game.js");
-  script.setAttribute("type", "text/javascript");
-  document.getElementsByTagName('head')[0].appendChild(script);
+  var gameScript = document.createElement("script");
+  gameScript.setAttribute("src", "game.js");
+  gameScript.setAttribute("type", "text/javascript");
+  document.getElementsByTagName('head')[0].appendChild(gameScript);
 }
