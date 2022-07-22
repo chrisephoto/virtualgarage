1702a736d373cf904e51f3681f3672b55f5acf41
Chris <chrisephotography@gmail.com>
2022-07-22 14:57:38 -0400

    Update init.js

--- init.js
+++ init.js
@@ -1,10 +1,14 @@
-//get url
-var hash = window.location.hash.substr(1);
+window.addEventListener("DOMContentLoaded", init);
 
-//update title
-document.title = hash;
-document.getElementById("page-title").innerHTML = hash;
+function init() {
+  //get url
+  var hash = window.location.hash.substr(1);
 
-//load parts list json
+  //load parts list json
+  
+  //update title
+  document.title = hash;
+  document.getElementById("page-title").innerHTML = hash;
 
-//load game.js
+  //load game.js
+}
