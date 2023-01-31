61e0fd25e2c19d67fbc5d4c635199192d298477c
Chris <chrisephotography@gmail.com>
2023-01-31 10:29:16 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 for (let i = 0; i < window.parts.length; i++) {
   target = document.querySelector("#options div");
-  target.innerHTML += "<details><summary><h2>Headlights</h2><span>Stock</span></summary><ul><li>Stock</li><li>Civic Type R</li></ul></details>";
   
-  console.log(window.parts[i].title);
+  options = "";
+  for (let j = 0; j < window.parts[i].options.length; j++) {
+    options += "<li>" + window.parts[i].options[j].description + "</li>";
+  }
+    
+  target.innerHTML += "<details><summary><h2>" + window.parts[i].title + "</h2><span>" + window.parts[i].option[0].description + "</span></summary><ul>" + options + "</ul></details>";
 }
