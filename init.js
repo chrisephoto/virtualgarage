1dccc3bf223134c12b58791d4558741476676ee0
Chris <chrisephotography@gmail.com>
2023-01-31 15:05:12 -0500

    Update init.js

--- init.js
+++ init.js
@@ -25,30 +25,30 @@ function init() {
 
 function updateTitle() {
   // update window and page title
-  while (window.title === undefined) {
-    if (window.title  === undefined) {}
-    else {
-      document.title = window.title
-      document.getElementById("page-title").innerHTML = window.title;
-    }
+  if (window.title  === undefined) {
+   updateTitle();
+  }
+  else {
+    document.title = window.title
+    document.getElementById("page-title").innerHTML = window.title;
   }
 }
 
 function updateOptions() {
   // populate options
-  while (window.parts === undefined) {
-    if (window.parts  === undefined) {}
-    else {
-      for (let i = 0; i < window.parts.length; i++) {
-        target = document.querySelector("#options div");
+  if (window.parts  === undefined) {
+    updateOptions()
+  }
+  else {
+    for (let i = 0; i < window.parts.length; i++) {
+      target = document.querySelector("#options div");
 
-        options = "";
-        for (let j = 0; j < window.parts[i].options.length; j++) {
-          options += "<li>" + window.parts[i].options[j].description + "</li>";
-        }
-
-        target.innerHTML += "<details><summary><h2>" + window.parts[i].title + "</h2><span>" + window.parts[i].options[0].description + "</span></summary><ul>" + options + "</ul></details>";
+      options = "";
+      for (let j = 0; j < window.parts[i].options.length; j++) {
+        options += "<li>" + window.parts[i].options[j].description + "</li>";
       }
+
+      target.innerHTML += "<details><summary><h2>" + window.parts[i].title + "</h2><span>" + window.parts[i].options[0].description + "</span></summary><ul>" + options + "</ul></details>";
     }
   }
 }
