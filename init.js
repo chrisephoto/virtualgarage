d9cb1480c710fe616f6cf792973841756661eafc
Chris <chrisephotography@gmail.com>
2023-01-31 12:38:35 -0500

    Update init.js

--- init.js
+++ init.js
@@ -16,10 +16,24 @@ function init() {
   head.appendChild(gameScript);
   
   setTimeout(updateTitle, 500)
+  setTimeout(updateOptions, 1000);
 }
 
 function updateTitle() {
   // update window and page title
   document.title = window.title
   document.getElementById("page-title").innerHTML = window.title;
+}
+
+function updateOptions() {
+  for (let i = 0; i < window.parts.length; i++) {
+    target = document.querySelector("#options div");
+
+    options = "";
+    for (let j = 0; j < window.parts[i].options.length; j++) {
+      options += "<li>" + window.parts[i].options[j].description + "</li>";
+    }
+
+    target.innerHTML += "<details><summary><h2>" + window.parts[i].title + "</h2><span>" + window.parts[i].options[0].description + "</span></summary><ul>" + options + "</ul></details>";
+  }
 }
