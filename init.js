d79bddb047b4f3942af35374cef3c6e3ec4fcbef
Chris <chrisephotography@gmail.com>
2023-01-31 08:55:56 -0500

    Update init.js

--- init.js
+++ init.js
@@ -3,29 +3,15 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   var hash = window.location.hash.substr(1);
   dataScriptURL = "data/" + hash + ".js";
-  loadScript(dataScriptURL, loadScript("game.js", updatePage));
-}
-
-function loadScript(url, callback)
-{
-  // Adding the script tag to the head as suggested before
+  
   var head = document.head;
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = url;
-
-  // Then bind the event to the callback function.
-  // There are several events for cross browser compatibility.
-  if (callback) {
-    script.onreadystatechange = callback;
-    script.onload = callback;
-  }
-
-  // Fire the loading
+  script.src = dataScriptURL;
   head.appendChild(script);
 }
 
-var updatePage = function() {
-  document.title = "title"
+function updatePage() {
+  document.title = title
   document.getElementById("page-title").innerHTML = title;
-};
+}
