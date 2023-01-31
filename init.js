0321f08e79af0e6dd2581a5ea30c8c9df53f368b
Chris <chrisephotography@gmail.com>
2023-01-31 10:01:00 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,19 +1,21 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  var hash = window.location.hash.substr(1);
-  dataScriptURL = "data/" + hash + ".js";
-  
   var head = document.head;
   
+  // define parameters for data script
+  var hash = window.location.hash.substr(1);
+  dataScriptURL = "data/" + hash + ".js";
   var dataScript = document.createElement('script');
   dataScript.src = dataScriptURL;
   head.appendChild(dataScript);
   
+  // define parameters for game script
   var gameScript = document.createElement('script');
   gameScript.src = "game.js";
   head.appendChild(gameScript);
   
+  // update window and page title
   document.title = window.title
   document.getElementById("page-title").innerHTML = window.title;
 }
